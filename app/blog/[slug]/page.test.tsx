5e463f34ff06a9f852c0c677344f8f0c6747f3e4
lhaq <lhaquoc@gmail.com>
2024-09-24 01:25:31 -0500

    fixed error

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -12,5 +12,5 @@ it("App Router: Works with dynamic route segments", () => {
 
 it('My-Page sholud be in the document', () => {
   render(<Page params={{ slug: "Test" }} />);
-  expect(screen.getByText('My-Page')).toBeInTheDocument
+  expect(screen.getByText('My-New-Page')).toBeInTheDocument
 });
